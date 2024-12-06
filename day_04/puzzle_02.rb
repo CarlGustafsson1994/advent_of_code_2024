2ebb54aa81247f438eb745fea570800f0b8cb829
Carl Gustafsson <79852889+CarlGustafsson1994@users.noreply.github.com>
2024-12-06 08:33:08 +0100

    Update Day 04 puzzle_02.rb

--- day_04/puzzle_02.rb
+++ day_04/puzzle_02.rb
@@ -1,7 +1,3 @@
-pad_arrays = ->(line_map) do
-  line_map.map { |line| ["."] + line + ["."] }.unshift(["."] * (a.first.length + 2)).push(["."] * (a.first.length + 2))
-end
-
 x_mas = ->(line_map) do
   sum = 0
   column_triples = line_map.map { _1.each_cons(3).to_a }
@@ -20,7 +16,4 @@ x_mas = ->(line_map) do
 end
 
 lines = File.readlines("./input_04.txt", chomp: true).map(&:chars)
-lines_transposed = lines.transpose
-padded_lines = pad_arrays.call(lines)
-padded_lines_tranposed = pad_arrays.call(lines_transposed)
-puts x_mas.call(padded_lines) + x_mas.call(padded_lines_tranposed)
+puts x_mas.call(lines) + x_mas.call(lines.transpose)
