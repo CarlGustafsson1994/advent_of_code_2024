b9f5cd6c1fcba45410d486983018166509e82557
Carl Gustafsson <79852889+CarlGustafsson1994@users.noreply.github.com>
2024-12-08 21:43:37 +0100

    Day 7 part 2.

---
+++ day_07/puzzle_02.rb
@@ -0,0 +1,20 @@
+equations = File.readlines("./input_07.txt", chomp: true).map do |line|
+  res, nums = line.split(":")
+  [res.to_i, nums.split(" ").map(&:to_i)]
+end
+
+equations.sum do |result, constants|
+  operator_permutations = ["+", "*", "||"].repeated_permutation(constants.length - 1)
+  equation_valid = operator_permutations.any? do |permutation|
+    sum = constants.first
+    constants[1..-1].each_with_index do |right, index|
+      if permutation[index] == "||"
+        sum = "#{sum}#{right}".to_i
+      else
+        sum = sum.send(permutation[index], right)
+      end
+    end
+    result == sum
+  end
+  equation_valid ? result : 0
+end.tap { puts _1 }
