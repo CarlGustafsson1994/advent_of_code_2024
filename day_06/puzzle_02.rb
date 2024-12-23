07a42f49024b6c46d16b62d7b5a0f13d3f29a8c5
Carl Gustafsson <79852889+CarlGustafsson1994@users.noreply.github.com>
2024-12-23 11:37:03 +0100

    Day 6 part 2 improvement.

--- day_06/puzzle_02.rb
+++ day_06/puzzle_02.rb
@@ -16,30 +16,39 @@ grid_copy = ->(grid) do
   grid.map { _1.map(&:dup) }
 end
 
-potential_obstacles = grid.each_with_index.with_object([]) do |(row, x), coordinates|
-  row.each_with_index do |cell, y|
-    coordinates << [x, y] if cell != "#" && cell != "^"
-  end
-end
-
 simulate_guard = ->(new_grid, current_location, direction_map, directions, next_location) do
   continue_loop = true
-  steps = 0
+  direction_hash = {}
+  infinite_loop = false
 
   while continue_loop
     new_x, new_y = next_location.call(current_location, direction_map[directions.peek])
-    if [new_x, new_y].any? { |cord| cord < 0 || cord > (new_grid.size - 1) } || steps > (new_grid.size * new_grid.size) * 4
+    if [new_x, new_y].any? { |cord| cord < 0 || cord > (new_grid.size - 1) }
       continue_loop = false
     else
       directions.next if new_grid[new_x][new_y] == "#"
       new_x2, new_y2 = next_location.call(current_location, direction_map[directions.peek])
       directions.next if new_grid[new_x2][new_y2] == "#"
       current_location = next_location.call(current_location, direction_map[directions.peek])
-      steps += 1
+      if direction_hash[current_location] == directions.peek
+        continue_loop = false
+        infinite_loop = true
+      else
+        direction_hash[current_location] = directions.peek
+      end
       new_grid[current_location.first][current_location.last] = "X"
     end
   end
-  steps
+  infinite_loop
+end
+
+first_run_grid = grid_copy.call(grid)
+first_run = simulate_guard.call(first_run_grid, current_location, direction_map, directions.cycle, next_location)
+
+potential_obstacles = first_run_grid.each_with_index.with_object([]) do |(row, x), coordinates|
+  row.each_with_index do |cell, y|
+    coordinates << [x, y] if [x, y] != guard_location && cell == "X"
+  end
 end
 
 time_loops = 0
@@ -47,8 +56,8 @@ potential_obstacles.each_with_index do |obstacle, index|
   new_grid = grid_copy.call(grid)
   new_grid[obstacle.first][obstacle.last] = "#"
 
-  steps = simulate_guard.call(new_grid, guard_location, direction_map, directions.cycle, next_location)
-  time_loops += 1 if steps > (new_grid.size * new_grid.size) * 4
+  is_infinite = simulate_guard.call(new_grid, guard_location, direction_map, directions.cycle, next_location)
+  time_loops += 1 if is_infinite
 end
 
 puts time_loops
